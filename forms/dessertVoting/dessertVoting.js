87ce946cece9708bc0c28c739ed938a9a2fd3d66
kes97391 <79420804+kes97391@users.noreply.github.com>
2021-04-02 11:12:12 -0500

    Initial commit

---
+++ forms/dessertVoting/dessertVoting.js
@@ -0,0 +1,19 @@
+var dessertList = ['Chocolate Cake','Cream Brulee','Cheesecake',
+                  'Keyline Pie','Cherry Cobbler','Chocolate Chip Cookies']
+
+dessertVoting.onshow=function(){
+    drpDesserts.clear()
+    for (i=0; < dessertList.length; i++)
+      drpDesserts.addItem(dessertList[i])
+}
+
+
+drpDesserts.onclick=function(){
+// this 'if' kicks user out if they  just clicked on control 
+// but not on one item in the list.
+    if (typeof(s) == "object")   
+      return                    
+    else {  // the user picked something
+        
+    }
+}
