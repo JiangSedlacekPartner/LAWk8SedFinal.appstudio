f01c494e36cc921baabd69e8297798ca935c5200
kes97391 <79420804+kes97391@users.noreply.github.com>
2021-04-02 20:04:15 -0500

    final commit

--- forms/mobileNav/mobileNav.js
+++ forms/mobileNav/mobileNav.js
@@ -1,8 +1,27 @@
 
-hmbMenu.onclick=function(s){
-  if (typeof(s) == "object") 
-    return
-  /* add a switch here to take the user to the form
-     they chose. 
-  */
+mobileNav.onshow=function(){
+  hmbMenu.hidden = false
 }
+
+hmbMenu.onclick=function(s){
+  if (typeof(s) == "object"){
+    return
+  }else{
+    switch(s) {
+      case "Dessert Voting":
+        ChangeForm(dessertVoting);
+        break;
+      case "Describe You":
+        ChangeForm(describeYou);
+        break;
+      case "Favorite Exercise":
+        ChangeForm(favExercise);
+        break;
+      case "Mobile Nav":
+        ChangeForm(mobileNav);
+        break;
+    }
+  }
+}
+
+
